3733d9934ab973fc324666ccd7123eec742eeddf
Andrew Rose <andrewrose@IT1942.local>
2022-05-20 21:18:31 -0700

    Adding source code

---
+++ App.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [modalIsVisable, setModalIsVIsble] = useState(false)
+  const [courseGoals, setCourseGoals] = useState([])
+
+  function startAddGoalHandler() {
+    setModalIsVIsble(true);
+  };
+
+  function endAddGoalHandler() {
+    setModalIsVIsble(false)
+  }
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      {text: enteredGoalText, id: Math.random().toString()}
+    ]);
+    endAddGoalHandler()
+  };  
+
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <StatusBar style='light'/>
+      <View style={styles.appContainer}>
+        <Button
+          title='Add New Goal'
+          color="#a065ec"
+          onPress={startAddGoalHandler}
+        />
+        <GoalInput
+          visible={modalIsVisable}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style = {styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return <GoalItem
+                        text={itemData.item.text}
+                        id={itemData.item.id}
+                        onDeleteItem={deleteGoalHandler}
+                      />;
+            }}
+            keyExtractor={(item, index) => {
+              return item.id
+            }}
+            alwaysBounceVertical={false} />
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+ appContainer: {
+   paddingTop: 50,
+   paddingHorizontal: 16,
+   flex: 1,
+ },
+ goalsContainer: {
+   flex: 4
+ },
+});
